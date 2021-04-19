3ee4b8e97d3c626175dcb828136d5ee94fc489e1
gracemakai <gracemakainjuguna@gmail.com>
2021-04-19 12:37:13 +0300

    Changed Ui for registration screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -18,6 +18,16 @@ class _RegisterScreenState extends State<RegisterScreen> {
         context, MaterialPageRoute(builder: (context) => LoginScreen()));
   }
 
+  Color getColorFromHex(String hexColor) {
+    hexColor = hexColor.replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF" + hexColor;
+    }
+    if (hexColor.length == 8) {
+      return Color(int.parse("0x$hexColor"));
+    }
+  }
+
   @override
   void dispose() {
     _nameTextController.dispose();
@@ -31,60 +41,181 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
+      backgroundColor: getColorFromHex("#0636F9"),
+      body: Center(
+        child: SingleChildScrollView(
           child: Container(
             width: 250,
             child: Padding(
-              padding: const EdgeInsets.only(top: 40),
+              padding: const EdgeInsets.only(top: 60),
               child: Column(
                 children: <Widget>[
-                  Image.asset(
-                    "assets/app_icon_plate.png",
-                    height: 100,
-                    width: 100,
+                  Text(
+                    "Welcome",
+                    style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        letterSpacing: 3),
                   ),
-                  TextField(
-                    controller: _nameTextController,
-                    decoration: InputDecoration(
-                      hintText: "Name",
-                      contentPadding: EdgeInsets.only(left: 10),
-                    ),
+                  SizedBox(
+                    height: 60,
                   ),
-                  TextField(
-                    controller: _emailTextController,
-                    decoration: InputDecoration(
-                      hintText: "Email",
-                      contentPadding: EdgeInsets.only(left: 10),
-                    ),
-                  ),
-                  TextField(
-                    controller: _phoneTextController,
-                    decoration: InputDecoration(
-                      hintText: "Phone",
-                      contentPadding: EdgeInsets.only(left: 10),
-                    ),
-                  ),
-                  TextField(
-                    controller: _passwordTextController,
-                    decoration: InputDecoration(
-                      hintText: "Password",
-                      contentPadding: EdgeInsets.only(left: 10),
-                    ),
-                  ),
-                  TextField(
-                    controller: _confirmPasswordTextController,
-                    decoration: InputDecoration(
-                      hintText: "Confirm password",
-                      contentPadding: EdgeInsets.only(left: 10),
+                  Container(
+                    child: Column(
+                      children: [
+                        TextField(
+                          style: TextStyle(color: Colors.white),
+                          controller: _nameTextController,
+                          decoration: InputDecoration(
+                            enabled: true,
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 1.0,
+                                    style: BorderStyle.solid,
+                                    color: Colors.white),
+                                borderRadius: BorderRadius.circular(10.0),
+                                gapPadding: 4),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 1.0,
+                                    style: BorderStyle.solid,
+                                    color: getColorFromHex("#F906B0")),
+                                borderRadius: BorderRadius.circular(10.0),
+                                gapPadding: 4),
+                            hintText: "Name",
+                            hintStyle: TextStyle(color: Colors.grey[500]),
+                            contentPadding: EdgeInsets.only(left: 10),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          style: TextStyle(color: Colors.white),
+                          controller: _emailTextController,
+                          decoration: InputDecoration(
+                            enabled: true,
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 1.0,
+                                    style: BorderStyle.solid,
+                                    color: Colors.white),
+                                borderRadius: BorderRadius.circular(10.0),
+                                gapPadding: 4),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 1.0,
+                                    style: BorderStyle.solid,
+                                    color: getColorFromHex("#F906B0")),
+                                borderRadius: BorderRadius.circular(10.0),
+                                gapPadding: 4),
+                            hintText: "Email",
+                            hintStyle: TextStyle(color: Colors.grey[500]),
+                            contentPadding: EdgeInsets.only(left: 10),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          style: TextStyle(color: Colors.white),
+                          controller: _phoneTextController,
+                          decoration: InputDecoration(
+                            enabled: true,
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 1.0,
+                                    style: BorderStyle.solid,
+                                    color: Colors.white),
+                                borderRadius: BorderRadius.circular(10.0),
+                                gapPadding: 4),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 1.0,
+                                    style: BorderStyle.solid,
+                                    color: getColorFromHex("#F906B0")),
+                                borderRadius: BorderRadius.circular(10.0),
+                                gapPadding: 4),
+                            hintText: "Phone",
+                            hintStyle: TextStyle(color: Colors.grey[500]),
+                            contentPadding: EdgeInsets.only(left: 10),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          style: TextStyle(color: Colors.white),
+                          controller: _passwordTextController,
+                          decoration: InputDecoration(
+                            enabled: true,
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 1.0,
+                                    style: BorderStyle.solid,
+                                    color: Colors.white),
+                                borderRadius: BorderRadius.circular(10.0),
+                                gapPadding: 4),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 1.0,
+                                    style: BorderStyle.solid,
+                                    color: getColorFromHex("#F906B0")),
+                                borderRadius: BorderRadius.circular(10.0),
+                                gapPadding: 4),
+                            hintText: "Password",
+                            hintStyle: TextStyle(color: Colors.grey[500]),
+                            contentPadding: EdgeInsets.only(left: 10),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          style: TextStyle(color: Colors.white),
+                          controller: _confirmPasswordTextController,
+                          decoration: InputDecoration(
+                            enabled: true,
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 1.0,
+                                    style: BorderStyle.solid,
+                                    color: Colors.white),
+                                borderRadius: BorderRadius.circular(10.0),
+                                gapPadding: 4),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    width: 1.0,
+                                    style: BorderStyle.solid,
+                                    color: getColorFromHex("#F906B0")),
+                                borderRadius: BorderRadius.circular(10.0),
+                                gapPadding: 4),
+                            hintText: "Confirm password",
+                            hintStyle: TextStyle(color: Colors.grey[500]),
+                            contentPadding: EdgeInsets.only(left: 10),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                   Padding(
                     padding: const EdgeInsets.only(top: 20.0),
                     child: ElevatedButton(
-                        child: Text("Register"), onPressed: () {}),
+                        child: Text(
+                          "Register",
+                          style: TextStyle(color: getColorFromHex("#000000")),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          primary: getColorFromHex("#F9C906"),
+                        ),
+                        onPressed: () {}),
                   ),
                   TextButton(
-                    child: Text("New user?"),
+                    child: Text(
+                      "New user?",
+                      style: TextStyle(color: getColorFromHex("#F906B0")),
+                    ),
                     onPressed: () {
                       _navigateToLoginScreen();
                     },
@@ -94,7 +225,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
             ),
           ),
         ),
-
+      ),
     );
   }
 }
+
+
